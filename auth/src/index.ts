9af1e9f1497452a570f1e0fd6c051512929adddf
as <you@example.com>
2020-12-19 20:55:53 +0700

    Add ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.get("/api/users/me", (req, res) => {
+  return res.send("Hi there");
+});
+
 app.listen(PORT, () =>
   console.info(`🚀️ [ Auth ] service is running on port ${PORT} ✈️`)
 );
