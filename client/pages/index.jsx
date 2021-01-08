0428c24a4446d1caa7aba4d1d4398d8f37515ee3
Nasrul Fuad <nasrulfuad08@gmail.com>
2021-01-08 21:38:55 +0700

    Show list tickets

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,15 +1,43 @@
 import Link from "next/link";
 
-const LandingPage = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in as {currentUser?.email}</h1>
-  ) : (
-    <h1>You are not signed in</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => (
+    <tr key={ticket.id}>
+      <td>{ticket.title}</td>
+      <td>{ticket.price}</td>
+      <td>
+        <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+          <a>Show</a>
+        </Link>
+      </td>
+    </tr>
+  ));
+
+  return (
+    <div className="card">
+      <div className="card-header">
+        <h1>Tickets</h1>
+      </div>
+      <div className="card-body">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Link</th>
+            </tr>
+          </thead>
+          <tbody>{ticketList}</tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  return {};
+  const { data } = await client.get("/api/tickets");
+
+  return { tickets: data };
 };
 
 export default LandingPage;
---
+++ client/pages/tickets/[ticketId].jsx
@@ -0,0 +1,34 @@
+import useRequest from "../../hooks/useRequest";
+
+const TicketShow = ({ ticket }) => {
+  const { doRequest, errors } = useRequest({
+    url: "/api/orders",
+    method: "post",
+    body: {
+      ticketId: ticket.id,
+    },
+    onSuccess: (order) => console.log(order),
+  });
+
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h1>{ticket.title}</h1>
+        <h4>Price : ${ticket.price}</h4>
+        {errors}
+        <button className="btn btn-primary" onClick={doRequest}>
+          Purchase
+        </button>
+      </div>
+    </div>
+  );
+};
+
+TicketShow.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+  return { ticket: data };
+};
+
+export default TicketShow;
